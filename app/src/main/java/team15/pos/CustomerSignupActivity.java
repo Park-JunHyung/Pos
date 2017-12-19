4d0c0c1695fb4ec16bfa9120777833754c44355a
Park JunHyung <schwazodiac@gmail.com>
2017-12-19 14:45:21 +0900

    first

---
+++ app/src/main/java/team15/pos/CustomerSignupActivity.java
@@ -0,0 +1,13 @@
+package team15.pos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_signup);
+    }
+}
