dff0386133b8b3167e0b91b7608df9200021c056
unknown <arcane1028@gmail.com>
2017-12-20 13:30:26 +0900

    ui 1 to 2 implement

--- app/src/main/java/team15/pos/MainActivity.java
+++ app/src/main/java/team15/pos/MainActivity.java
@@ -1,7 +1,12 @@
 package team15.pos;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button runPaymentBtn = (Button)findViewById(R.id.runPayment);
+
+        runPaymentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder paymentDialog = new AlertDialog.Builder(MainActivity.this);
+                final CharSequence[] paymentType = {"현금결제", "카드결제", "포인트 결제"};
+
+                DialogInterface dialogInterface= paymentDialog.setTitle("결제 선택")
+                        .setItems(paymentType, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, paymentType[i].toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("x", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Toast.makeText(MainActivity.this, "종료",Toast.LENGTH_SHORT).show();
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 }
