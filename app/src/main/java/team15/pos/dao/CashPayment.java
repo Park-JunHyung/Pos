21af7e0c3ba678f6e144b1ee7ee779410c9f5797
Park JunHyung <schwazodiac@gmail.com>
2017-12-21 20:45:30 +0900

    Cash payment done

--- app/src/main/java/team15/pos/dao/CashPayment.java
+++ app/src/main/java/team15/pos/dao/CashPayment.java
@@ -1,8 +1,15 @@
 package team15.pos.dao;
 
+import android.content.Context;
+
 /**
  * Created by JSH on 2017-12-20.
  */
 
 public class CashPayment {
+
+    public void setTypeAndPayment(int getPayment, Context context){
+        new PaymentDAO().createPayment(getPayment,"현금",context);
+    }
+
 }
--- app/src/main/java/team15/pos/dto/Payment.java
+++ app/src/main/java/team15/pos/dto/Payment.java
@@ -1,6 +1,8 @@
 package team15.pos.dto;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Created by Schwa on 2017-12-20.
@@ -9,16 +11,25 @@ import java.util.ArrayList;
 public class Payment {
     private String paymentNumber;
     private int paymentPrice;
-    private ArrayList<Product> paymentProductList;
+    //    private ArrayList<Product> paymentProductList;
+    private HashMap<String, ArrayList<Product>> arrayListMap=new HashMap<>();
     private String paymentType;
-    private String paymentDate;
+    private Date paymentDate;
 
-    public Payment(String paymentNumber, int paymentPrice, ArrayList<Product> paymentProductList, String paymentType, String paymentDate) {
+    public Payment(String paymentNumber, int paymentPrice, ArrayList<Product> paymentProductList, String paymentType, Date paymentDate) {
         this.paymentNumber = paymentNumber;
         this.paymentPrice = paymentPrice;
-        this.paymentProductList = paymentProductList;
+        this.arrayListMap.put("t", paymentProductList);
         this.paymentType = paymentType;
         this.paymentDate = paymentDate;
+    }
+
+    public ArrayList<Product> getPaymentProductList() {
+        return arrayListMap.get("t");
+    }
+
+    public void setArrayListMap(ArrayList<Product> arrayListMap) {
+        this.arrayListMap.put("t", arrayListMap);
     }
 
     public String getPaymentNumber() {
@@ -37,14 +48,6 @@ public class Payment {
         this.paymentPrice = paymentPrice;
     }
 
-    public ArrayList<Product> getPaymentProductList() {
-        return paymentProductList;
-    }
-
-    public void setPaymentProductList(ArrayList<Product> paymentProductList) {
-        this.paymentProductList = paymentProductList;
-    }
-
     public String getPaymentType() {
         return paymentType;
     }
@@ -53,11 +56,11 @@ public class Payment {
         this.paymentType = paymentType;
     }
 
-    public String getPaymentDate() {
+    public Date getPaymentDate() {
         return paymentDate;
     }
 
-    public void setPaymentDate(String paymentDate) {
+    public void setPaymentDate(Date paymentDate) {
         this.paymentDate = paymentDate;
     }
 }
